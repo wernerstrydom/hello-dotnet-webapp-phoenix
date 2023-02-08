ad16ed01f913040aa56176f62ff21c403db2e966
Werner Strydom <bloudraak@users.noreply.github.com>
2023-02-08 06:46:24 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello
+
+This repository contains a overly simple web application, written in .NET, that displays "Hello World". It serves as a template for testing various approaches to the software release process, as it pertains to .NET.
---
+++ src/Hello.FunctionalTests/PingTest.cs
@@ -0,0 +1,18 @@
+namespace Hello.FunctionalTests;
+
+public class PingTest
+{
+    [Fact]
+    public async Task Ping()
+    {
+        var url = Environment.GetEnvironmentVariable("WEBAPP_URL");
+        Assert.NotNull(url);
+        Assert.StartsWith("http", url);
+        var client = new HttpClient();
+        var response = await client.GetAsync(url);
+        Assert.True(response.IsSuccessStatusCode);
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.NotNull(content);
+        Assert.Contains("Hello World", content);
+    }
+}
